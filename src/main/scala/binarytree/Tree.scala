6eae0912826f858e4663ee5570f046086f3d0039
alanp <alanphillips78@gmail.com>
2017-05-23 21:28:45 +0100

    Scala BinaryTree

---
+++ src/main/scala/binarytree/Tree.scala
@@ -0,0 +1,49 @@
+package binarytree
+
+
+trait Tree[+T]
+
+case object Empty extends Tree[Nothing]
+
+case class Node[T](elem: T, left: Tree[T], right: Tree[T]) extends Tree[T]
+
+object Tree {
+
+  def preOrderList[T](t: Tree[T]): List[T] = t match {
+    case Empty => Nil
+    case Node(e, left, right) => e :: preOrderList(left) ::: preOrderList(right)
+  }
+
+  def postOrderList[T](t: Tree[T]): List[T] = t match {
+    case Empty => Nil
+    case Node(e, left, right) => postOrderList(left) ::: postOrderList(right) ::: List(e)
+  }
+
+  def inOrderList[T](t: Tree[T]): List[T] = t match {
+    case Empty => Nil
+    case Node(e, left, right) => inOrderList(left) ::: List(e) ::: inOrderList(right)
+  }
+
+  def size[T](t: Tree[T]): Int = t match {
+    case Empty => 0
+    case Node(e, left, right) => 1 + size(left) + size(right)
+  }
+
+  def leafCount[T](t: Tree[T]): Int = t match {
+    case Empty => 0
+    case Node(e, Empty, Empty) => 1
+    case Node(e, left, right) => leafCount(left) + leafCount(right)
+  }
+
+  def leaves[T](t: Tree[T]): List[T] = t match {
+    case Empty => Nil
+    case Node(e, Empty, Empty) => List(e)
+    case Node(e, left, right) => leaves(left) ::: leaves(right)
+  }
+
+  def height[T](t: Tree[T]): Int = t match {
+    case Empty => 0
+    case Node(e, left, right) => 1 + math.max(height(left), height(right))
+  }
+
+}
